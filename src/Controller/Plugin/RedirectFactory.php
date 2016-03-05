2f7429ccf90ca4d25c8e5f27537baa705b1f0d1f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-03-05 14:55:15 +0700

    isset check

--- src/Controller/Plugin/RedirectFactory.php
+++ src/Controller/Plugin/RedirectFactory.php
@@ -28,9 +28,16 @@ class RedirectFactory
         $services = $manager->getServiceLocator();
         $config   = $services->get('config');
 
+        if (! isset($config['redirect_handler_module'])) {
+            $config['redirect_handler_module'] = [
+                'allow_not_routed_url' => false,
+                'default_url' => '/'
+            ];
+        }
+
         return new Redirect(
             $config['redirect_handler_module'],
-            $manager
+            $manager // I KNOW, on PURPOSE!
         );
     }
 }
