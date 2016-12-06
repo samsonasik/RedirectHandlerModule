2ffc30a5ce680250606c83f8f2a2263b481fdb1f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-12-07 05:25:26 +0700

    bug fixes: same route name with different Query parameter should be redirected

--- test/Controller/Plugin/RedirectTest.php
+++ test/Controller/Plugin/RedirectTest.php
@@ -396,4 +396,66 @@ class RedirectTest extends PHPUnit_Framework_TestCase
 
         $this->redirect->toUrl($url);
     }
+
+    public function testSameUrlWithDifferentQueryParametersShouldBeRedirected()
+    {
+        $this->redirect = new Redirect(
+            [
+                'allow_not_routed_url' => false,
+                'default_url' => '/',
+            ],
+            $this->controllerManager->reveal()
+        );
+
+        $url = '/bar?succes=1';
+
+        $request = $this->prophesize(Request::class);
+        $request->getRequestUri()->willReturn('/bar')->shouldBeCalled();
+        $request->setUri($url)->shouldBeCalled();
+        $this->controller->getRequest()->willReturn($request);
+
+        $mvcEvent = $this->prophesize(MvcEvent::class);
+        $routeMatch = $this->prophesize(RouteMatch::class);
+        if (class_exists(RouteMatch::class)) {
+            $routeMatch = $this->prophesize(RouteMatch::class);
+        } else {
+            $routeMatch = $this->prophesize(V2RouteMatch::class);
+        }
+        $routeMatch->getMatchedRouteName()->willReturn('bar')->shouldBeCalled();
+        $routeMatch->getParam('action')->willReturn('bar')->shouldBeCalled();
+        $mvcEvent->getRouteMatch()->willReturn($routeMatch);
+
+        if (interface_exists(RouteInterface::class)) {
+            $router = $this->prophesize(TreeRouteStack::class);
+        } else {
+            $router = $this->prophesize(V2TreeRouteStack::class);
+        }
+        $router->getRequestUri()->willReturn('http://localhost/bar');
+
+        if (class_exists(RouteMatch::class)) {
+            $match = $this->prophesize(RouteMatch::class);
+        } else {
+            $match = $this->prophesize(V2RouteMatch::class);
+        }
+
+        $match->getParam('controller')->willReturn('bar')->shouldBeCalled();
+        $match->getParam('middleware')->willReturn(false)->shouldBeCalled();
+        $match->getMatchedRouteName()->willReturn('bar')->shouldBeCalled();
+        $match->getParam('action')->willReturn('bar')->shouldBeCalled();
+
+        $router->match($request)->willReturn($match);
+        $mvcEvent->getRouter()->willReturn($router);
+
+        $response = $this->prophesize(Response::class);
+        $mvcEvent->getResponse()->willReturn($response);
+
+        $headers = $this->prophesize(Headers::class);
+        $headers->addHeaderLine('Location', $url);
+        $response->getHeaders()->willReturn($headers);
+        $response->setStatusCode(302)->shouldBeCalled();
+
+        $this->controller->getEvent()->willReturn($mvcEvent);
+        $this->redirect->setController($this->controller->reveal());
+        $this->redirect->toUrl($url);
+    }
 }
